59f28878a20710a38cfe89c99cdb5ddd12403780
Jonatan laverde <jonathanlaverde@hotmail.com>
2023-09-22 14:25:50 -0500

    Primer commit

---
+++ src/main/java/B1/CLASE1.java
@@ -0,0 +1,15 @@
+package B1;
+
+public class CLASE1 {
+
+    public static void main(String[] args) {
+        System.out.println(saludar("mauricio"));
+    }
+
+
+    public static String saludar(String nombre){
+        return "Hola "+nombre;
+    }
+
+
+}
---
+++ src/main/java/B1/CLASE2.java
@@ -0,0 +1,36 @@
+package B1;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class CLASE2 {
+    public static void main(String[] args) {
+        // declaracion, inicializacion y uso;
+
+        //Objetos o Wrapper
+
+        //Cadena de caracter
+        String valor1; // si esta de esta forma la variable es null
+        String valor2="Mauricio";//Variable inicializada
+
+        Date horaActual;//declaracion
+        Date horaSiguiente=new Date();
+
+
+
+        //Datos primitivos
+        //numero
+        int num1=0;
+        long numLargo=1000;
+
+        //Valores decimales
+        double num2=0.1;
+
+        //boleanos...logicos
+        boolean logico=true;
+        boolean logico2=false;
+
+        char val3=´a´
+
+    }
+}
