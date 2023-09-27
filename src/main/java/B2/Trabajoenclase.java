c0fd8559bdf7a9e50cdabdcb612ffdc1735b8dae
Jlaverde1036 <jonathanlaverde@hotmail.com>
2023-09-26 20:06:33 -0500

    Arreglos

--- src/main/java/B2/Trabajoenclase.java
+++ src/main/java/B2/Trabajoenclase.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 public class Trabajoenclase {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Por favor digite el valor en pesos");
+        Scanner ingreso = new Scanner(System.in);
+
+
+        double dolar, peso;
+        System.out.println("Por favor digite el valor en Pesos");
+
+        peso = ingreso.nextDouble();
+        dolar = peso   * 4020.50;
+
+        System.out.println("El valor en Dolares es"+ dolar);
+
+
+
+
+    }
+
+
+
 
 
 
@@ -21,7 +37,7 @@ public class Trabajoenclase {
 
 
 
-    }
+
 
 
 
